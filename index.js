100c5b45c80182d37edbaa80e47241618a8cfc06
Prabhat <prabhatdbg21@gmail.com>
2022-12-29 12:03:55 +0530

    complete task

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ console.log(moment().format('dddd'));
 console.log(moment().format("MMM Do YY"));
 
 // Check if the following date is valid or not
-console.log(moment("2020-01-01", "YYYY-MM-DD").isValid());
-console.log(moment("2020-14-01", "YYYY-MM-DD").isValid());
+console.log(moment().isValid("2020-01-01"));
+console.log(moment().isValid("2020-14-01"));
 
 // Manipulating Dates
 console.log(moment().add(7, 'days').calendar());
@@ -38,28 +38,45 @@ console.log(moment([2020]).isLeapYear());
 
 
 
-const lodash  = require("lodash");
+// const lodash  = require("lodash");    OR
+// import lodash from "lodash" ;
+import _ from "lodash" ;
+
+// Array
 
 let characters = ["a", "b", "c", "d"];
-
-let chunked = lodash.chunk(characters, 2);
-
+let chunked = _.chunk(characters, 2);
 console.log(chunked);
 
-console.log(lodash.difference([2, 1], [2, 3]));
+console.log(_.difference([2, 1], [2, 3]));
 
-console.log(lodash.compact([0, 1, false, 2, '', 3])); // Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+console.log(_.compact([0, 1, false, 2, '', 3])); // Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
 
-console.log(lodash.drop(characters, 2));  // remove first 2 from array
+console.log(_.drop(characters, 2));  // remove first 2 from array
 
-console.log(lodash.dropRight(characters, 2));  // remove last 2 from array
+console.log(_.dropRight(characters, 2));  // remove last 2 from array
 
+// Methods
 
+console.log(_.countBy([6.1, 4.2, 6.3], Math.floor));
 
 var users = [
     { 'user': 'barney',  'age': 36, 'active': true },
     { 'user': 'fred',    'age': 40, 'active': false },
     { 'user': 'pebbles', 'age': 1,  'active': true },
 ];
+console.log(_.filter(users, function(o) { return !o.active; }));
 
-/* console.log(users.find(users, function(o) { return o.age < 40; })); */
+console.log(_.find(users, function(o) { return o.age < 40; }));
+
+console.log(_.forEach([1, 2], function(value) {
+    console.log(value);
+  })
+  );
+function duplicate(n) {
+    return [[[n, n]]];
+}
+
+console.log(_.flatMapDeep([1, 2], duplicate));
+
+
